159c51b59064306c09402f275e6674cbf86e408a
thanos <atha0043@edu.eal.dk>
2016-05-03 10:16:35 +0200

    MVC

---
+++ DaxiomaticWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DaxiomaticWebApplication.Startup))]
+namespace DaxiomaticWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
